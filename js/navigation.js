e2caf147d15a91e39f26c124affb9b5976440245
Joey van der Linden <95914820+Jojopro56@users.noreply.github.com>
2024-04-09 20:45:13 +0200

    1.0
    Eerste upload

---
+++ js/navigation.js
@@ -0,0 +1,4 @@
+document.getElementById("portfolio").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = "portfolio.html"; // Navigate to portfolio.html
+});
