240557e4448720f5eb4e51507cce3da586dbac8f
Joey van der Linden <95914820+Jojopro56@users.noreply.github.com>
2024-04-10 20:34:33 +0200

    1.1
    fotografie in progress

---
+++ js/navigation-photography.js
@@ -0,0 +1,4 @@
+document.getElementById("viva").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = "viva.html"; // Navigate to portfolio.html
+});
---
+++ js/navigation-portfolio.js
@@ -0,0 +1,4 @@
+document.getElementById("photography").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = "photography.html"; // Navigate to portfolio.html
+});
