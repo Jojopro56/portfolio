7d12ca5525e278438cae55a54c5c695200f70f2f
Joey van der Linden <95914820+Jojopro56@users.noreply.github.com>
2024-04-11 12:49:21 +0200

    1.3
    - fotografie af
    - video bereikbaar

--- js/navigation-portfolio.js
+++ js/navigation-portfolio.js
@@ -2,3 +2,8 @@ document.getElementById("photography").addEventListener("click", function(event)
     event.preventDefault(); // Prevent default link behavior
     window.location.href = "photography.html"; // Navigate to portfolio.html
 });
+
+document.getElementById("videography").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = "videography.html"; // Navigate to portfolio.html
+});
