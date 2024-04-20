1910481862d249fd7078e5e00f7e553f05d238f3
Joey van der Linden <95914820+Jojopro56@users.noreply.github.com>
2024-04-20 23:07:27 +0200

    1.5.2
    - Alle knoppen werken
    - Wel nog niet goed gelinkt
    - Content mist nog

--- js/navigation.js
+++ js/navigation.js
@@ -7,3 +7,14 @@ document.getElementById("latest-work").addEventListener("click", function(event)
     event.preventDefault(); // Prevent default link behavior
     window.location.href = "blikveld.html"; // Navigate to portfolio.html
 });
+
+document.getElementById("about-me").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = "about.html"; // Navigate to portfolio.html
+});
+
+document.getElementById("contact").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = "contact.html"; // Navigate to portfolio.html
+});
+
