7cacad2af35bd8d42ce2ae2e0cb71b2960a901e8
Joey van der Linden <95914820+Jojopro56@users.noreply.github.com>
2024-04-22 13:45:03 +0200

    1.5.7
    - video af

--- js/navigation-videography.js
+++ js/navigation-videography.js
@@ -22,3 +22,8 @@ document.getElementById("vestingVideo").addEventListener("click", function(event
     event.preventDefault(); // Prevent default link behavior
     window.location.href = "vesting.html"; // Navigate to portfolio.html
 });
+
+document.getElementById("consumptie").addEventListener("click", function(event) {
+    event.preventDefault(); // Prevent default link behavior
+    window.location.href = "consumptie.html"; // Navigate to portfolio.html
+});
